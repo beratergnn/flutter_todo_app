bce31d365501a5fca3dc2287de21902785d02993
Berat <58957696+beratergnn@users.noreply.github.com>
2023-10-24 02:03:01 +0300

    first commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color backGroundColor = Color(0xFF292d37);
+
+  static const Color iconColor = Color(0xFF353f54);
+
+  static const Color textColor1 = Color(0xFF1c0c26);
+  static const Color textColor2 = Color(0xFF353f54);
+  static const Color textColor3 = Color(0xFFf0f8fe);
+
+  static const Color todoColor1 = Color(0xFFecefff);
+  static const Color todoColor2 = Color(0xFFd3e3ff);
+  static const Color todoColor3 = Color(0xFF959ca2);
+  static const Color todoColor4 = Color(0xFF44747c);
+}
